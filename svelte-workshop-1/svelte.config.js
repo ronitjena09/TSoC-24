403d389135c429898cec9f125ece1304cc888228
ronitjena09 <ronitjena64@gmail.com>
2024-07-07 10:25:34 +0530

    regular commit

--- svelte-workshop-1/svelte.config.js
+++ svelte-workshop-1/svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: preprocess()
 };
 
 export default config;
